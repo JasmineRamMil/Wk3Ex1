b452729f8ed4794512ec246802ae47952fe8abff
Jasmine Ramirez-Miller <jassymill@yahoo.com>
2025-02-02 23:50:59 -0600

    updating

--- Wk3Ex1/Program.cs
+++ Wk3Ex1/Program.cs
@@ -10,7 +10,27 @@ namespace Wk3Ex1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            //Ask user to enter the numeric grade
+            Console.Write("What is the Numeric score(0-100) ?");
+
+            //Convert input to double 
+            double grade = Convert.ToDouble(Console.ReadLine());
+
+            //Getting the letterGrade
+            string letterGrade;
+            if (grade >= 89.5)
+                letterGrade = "A";
+            else if (grade >= 79.5)
+                letterGrade = "B";
+            else if (grade >= 69.5)
+                letterGrade = "C";
+            else if (grade >= 59.5)
+                letterGrade = "D";
+            else
+                letterGrade = "F";
+            //Display the lettergrade
+            Console.WriteLine("Your letter grade is:" + letterGrade);
+
         }
     }
 }
